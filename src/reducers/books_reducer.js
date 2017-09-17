ef236fb051b0132a0260630762dfb60d0df30bb8
Manoj <manoj.delhi24@gmail.com>
2017-09-17 13:12:48 +0530

    booklist container created. connect added to connect books "store" and to BookList container.

---
+++ src/reducers/books_reducer.js
@@ -0,0 +1,10 @@
+export default function() {
+  return ( [
+      {title: "Sit amet: consectetur adipisicing elit. sed do eiusmod"},
+      {title: "Tempor incididunt ut labore et dolore magna aliqua"},
+      {title: "Ut enim ad minim veniam: anim id est laborum"},
+      {title: "sunt in culpa qui officia deserunt mollit"},
+    ]
+  );
+}
+
