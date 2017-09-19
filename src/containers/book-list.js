64b45d920e3726a34a78fced7423712e72a9faad
Manoj <manoj.delhi24@gmail.com>
2017-09-19 20:45:43 +0530

    bindActionCreators introduced to the system. Added a sample action creator and attached to the containers through dispatch

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends React.Component {
 
   renderList() {
     return (
       this.props.books.map((book) => (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => { this.props.selectBook(book) } }
+          className="list-group-item">
           {book.title}
         </li>
       ))
@@ -29,5 +34,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this fucntion will end up as props on the Booklist container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all
+  // of our reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 
